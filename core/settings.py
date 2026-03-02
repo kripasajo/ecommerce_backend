8ddbaafbcf5f4dfb61c0b8e2d4f9bff3891d92c1
kripasajo200@gmail.com <kripa sajo>
2026-03-02 23:04:29 +0530

    installed drf and added to installed_apps in settings.py

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',                        #custom user app
+    'accounts',  #custom user app
+    'rest_framework',                      
 ]
 
 MIDDLEWARE = [
