b9945f6e5d67354770b27dabdd0c7bf60950980d
kripasajo200@gmail.com <kripa sajo>
2026-03-25 21:05:20 +0530

    feat: add filtering, search, and ordering to product API

--- products/views.py
+++ products/views.py
@@ -2,6 +2,8 @@ from rest_framework.generics import ListCreateAPIView
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.permissions import AllowAny
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 class ProductListCreateView(ListCreateAPIView):
@@ -9,3 +11,7 @@ class ProductListCreateView(ListCreateAPIView):
     serializer_class = ProductSerializer
     permission_classes = [AllowAny]   # ✅ ADD THIS TEMPORARILY
 
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['category']
+    search_fields = ['name', 'description']
+    ordering_fields = ['price', 'created_at']
